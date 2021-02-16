48ab4a1cfab173124c3e0284f0116b02a763ff6c
Nickolay Sheika <mykola.sheika@deliveryhero.com>
2021-02-16 10:34:51 +0100

    test project

---
+++ StaticLibTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StaticLibTest
+//
+//  Created by Mykola Sheika on 16.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
